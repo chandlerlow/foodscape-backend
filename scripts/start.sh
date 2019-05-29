837a4b24015f6798cbc8c0d073630e44f4f1eae7
Kelvin Zhang <me@iamkelv.in>
2019-05-29 02:19:47 +0100

    Ensure web containers are persisted in TravisCI [kcz17]

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,6 @@
 
 if [ "$NODE_ENV" == "production" ] ; then
   npm run start
-elif [ "$NODE_ENV" == "test" ] ; then
-  # Don't do anything, leaving CI pipeline to execute test script directly
-  echo "Test environment"
 else
   npm run dev
 fi
