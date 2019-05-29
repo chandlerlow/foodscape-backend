ebe9ff6effb9986e6bb7c72ed166bc194cbef8cb
Kelvin Zhang <me@iamkelv.in>
2019-05-29 03:40:48 +0100

    Fix relative path in deploy.sh [kcz17]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ../.travis/id_rsa # Allow read access to the private key
-ssh-add ../.travis/id_rsa # Add the private key to SSH
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
 echo -e "Host $IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 git config --global push.default matching
