ac1dcec138879b5985d02a3323565889b8673fbf
Fawaz Shah <fawaz010@gmail.com>
2019-05-30 00:55:02 +0100

    Add seeder for Jenny [fs2217]

---
+++ db/seeders/20190529234409-jenny.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var jenny = {
+  name: 'Jenny Xu',
+  location: 'Block A',
+  phone_no: 07790727425
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Add dummy user Jenny
+    return queryInterface.bulkInsert('Users', [jenny], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // Delete user Jenny
+    return queryInterface.bulkDelete('Users', jenny, {});
+  }
+};
