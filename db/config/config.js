bb5e2851e0b883b884a9503cc641e4f19959f5cd
Kelvin Zhang <me@iamkelv.in>
2019-05-29 01:54:14 +0100

    Add environment variables to docker-compose files [kcz17]

--- db/config/config.js
+++ db/config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": null,
-    "database": "milkroad",
-    "host": "127.0.0.1",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
-    "username": "postgres",
-    "password": null,
-    "database": "milkroad",
-    "host": "127.0.0.1",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
