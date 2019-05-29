042518fbc6b1eace889fd315e7cc4ecac587bcbd
Kelvin Zhang <me@iamkelv.in>
2019-05-29 01:04:48 +0100

    Add test case in start.sh [kcz17]

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,7 @@ if [ "$NODE_ENV" == "production" ] ; then
   npm run start
 elif [ "$NODE_ENV" == "test" ] ; then
   # Don't do anything, leaving CI pipeline to execute test script directly
+  echo "Test environment"
 else
   npm run dev
 fi
