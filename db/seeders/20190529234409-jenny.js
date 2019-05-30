8db91eb982fb1887b0dcdf913399185037dd153a
Fawaz Shah <fawaz010@gmail.com>
2019-05-30 09:47:19 +0100

    Fix syntax error in object initialization [fs2217]

--- db/seeders/20190529234409-jenny.js
+++ db/seeders/20190529234409-jenny.js
@@ -3,10 +3,10 @@
 var jenny = {
   name: 'Jenny Xu',
   location: 'Block A',
-  phone_no: '07790727425'
-  createdAt = new Date(),
-  updatedAt = new Date()
-}
+  phone_no: '07790727425',
+  createdAt: new Date(),
+  updatedAt: new Date()
+};
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
