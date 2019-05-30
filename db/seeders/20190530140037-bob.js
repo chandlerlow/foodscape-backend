7230a6aad191d9edd17affebd18508ef7ef494a1
Fawaz Shah <fawaz010@gmail.com>
2019-05-30 15:58:21 +0100

    Create seeders for Bob (user) and Bananas (item) [fs2217]

---
+++ db/seeders/20190530140037-bob.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var bob = {
+  name: 'Bob Pintos',
+  location: 'Block C',
+  phone_no: '07831946183',
+  createdAt: new Date(),
+  updatedAt: new Date()
+};
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Add dummy user Bob
+    return queryInterface.bulkInsert('Users', [bob], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // Delete user Bob 
+    return queryInterface.bulkDelete('Users', bob, {});
+  }
+};
---
+++ db/seeders/20190530140526-bananas.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var bananas = {
+  name: 'Bananas',
+  photo: '',
+  quantity: '1 bunch',
+  expiry_date: new Date("June 2 2019 00:00"), 
+  description: 'A perfectly good bunch of bananas!',
+  createdAt: new Date(),
+  updatedAt: new Date()
+};
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Add dummy bananas item
+    return queryInterface.bulkInsert('Items', [bananas], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // Delete bananas item
+    return queryInterface.bulkDelete('Items', bananas, {});
+  }
+};
