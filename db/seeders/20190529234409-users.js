19fdb7c9a4a902841af0c98f0ef3198a090fd579
Fawaz Shah <fawaz010@gmail.com>
2019-06-13 18:05:03 +0100

    Add extra seed data [fs2217]

--- db/seeders/20190529234409-users.js
+++ db/seeders/20190529234409-users.js
@@ -10,8 +10,8 @@ const jenny = {
 };
 
 const bob = {
-  name: 'Bob Pintos',
-  location: 'Block C',
+  name: 'Bob Bulmer',
+  location: 'Block B',
   phone_no: '07831946183',
   username: 'bob',
   password: '$2b$12$UE.JXo9/jd.9bvIb.Aa.9.N6o8LpVW8nqsGQiBcD5sDvUktdfW5ze', // "password"
@@ -20,9 +20,42 @@ const bob = {
   updatedAt: new Date(),
 };
 
+const charlie = {
+  name: 'Charlie Cramer',
+  location: 'Block C',
+  phone_no: '07780372947',
+  username: 'charlie',
+  password: '$2b$12$UE.JXo9/jd.9bvIb.Aa.9.N6o8LpVW8nqsGQiBcD5sDvUktdfW5ze', // "password"
+  is_staff: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const daniel = {
+  name: 'Daniel Doberman',
+  location: 'Block D',
+  phone_no: '07680134830',
+  username: 'daniel',
+  password: '$2b$12$UE.JXo9/jd.9bvIb.Aa.9.N6o8LpVW8nqsGQiBcD5sDvUktdfW5ze', // "password"
+  is_staff: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const ellie = {
+  name: 'Ellie Enfield',
+  location: 'Block E',
+  phone_no: '07281715492',
+  username: 'elie',
+  password: '$2b$12$UE.JXo9/jd.9bvIb.Aa.9.N6o8LpVW8nqsGQiBcD5sDvUktdfW5ze', // "password"
+  is_staff: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
 module.exports = {
   up: queryInterface => queryInterface
-    .bulkInsert('Users', [jenny, bob], {}),
+    .bulkInsert('Users', [jenny, bob, charlie, daniel, ellie], {}),
 
   down: queryInterface => queryInterface
     .bulkDelete('Users', null, {}),
--- db/seeders/20190530140526-items.js
+++ db/seeders/20190530140526-items.js
@@ -1,11 +1,11 @@
 const moment = require('moment');
 
-const bobBananas = {
+const bobsBananas = {
   name: 'Bananas',
-  photo: 'bananas.jpg',
+  photo: 'bananas.png',
   quantity: '1 bunch',
-  expiry_date: moment().add(3, 'days').toDate(),
-  description: 'A perfectly good bunch of bananas!',
+  expiry_date: moment().add(1, 'days').toDate(),
+  description: 'A ripe bunch of bananas!',
   is_collected: false,
   user_id: 2,
   category_id: 2,
@@ -13,9 +13,35 @@ const bobBananas = {
   updatedAt: new Date(),
 };
 
+const charliesSteak = {
+  name: 'Beef Steak',
+  photo: 'steak.png',
+  quantity: '1 fillet',
+  expiry_date: moment().add(3, 'days').toDate(),
+  description: 'Extra mature beef steak',
+  is_collected: false,
+  user_id: 3,
+  category_id: 4,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const danielsChocolate = {
+  name: 'Galaxy Bar',
+  photo: 'galaxy.jpg',
+  quantity: '1 bar',
+  expiry_date: moment().add(7, 'days').toDate(),
+  description: 'A galaxy bar',
+  is_collected: false,
+  user_id: 4,
+  category_id: 7,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
 module.exports = {
   up: queryInterface => queryInterface
-    .bulkInsert('Items', [bobBananas], {}),
+    .bulkInsert('Items', [bobsBananas, charliesSteak, danielsChocolate], {}),
 
   down: queryInterface => queryInterface
     .bulkDelete('Items', null, {}),
